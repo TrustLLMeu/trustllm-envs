be80e08b84e8b654c4e74b38c1a6b5391981b40c
janEbert <janpublicebert@posteo.net>
2024-04-01 17:48:13 +0200

    Improve error handling

--- llm-foundry-env/apptainer_run.sh
+++ llm-foundry-env/apptainer_run.sh
@@ -8,6 +8,17 @@ _curr_file="${BASH_SOURCE[0]:-${(%):-%x}}"
 _curr_dir="$(dirname "$_curr_file")"
 source "$_curr_dir"/outside-container-scripts/activate.sh
 
+if ! [ -f "$apptainer_file" ]; then
+    echo 'Cannot find Apptainer container file; please run' \
+         "\`bash outside-container-scripts/build_apptainer.sh\`."
+    exit 1
+fi
+if ! [ -f "$scratch_dir"/triton-build-patch.py ]; then
+    echo 'Cannot find necessary patch file; please run' \
+         "\`bash outside-container-scripts/set_up_apptainer.sh\`."
+    exit 1
+fi
+
 apptainer run --nv --env PYTHONPATH= \
           --bind "$scratch_dir"/triton-build-patch.py:/usr/lib/python3/dist-packages/triton/common/build.py \
           "$apptainer_file" bash -c "
