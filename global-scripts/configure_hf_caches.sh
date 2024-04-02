8a4288ff59c30ef556f8f8edf8f0c780663e6247
janEbert <janpublicebert@posteo.net>
2024-04-02 22:29:33 +0200

    Comment out deprecated HF cache envvar

--- global-scripts/configure_hf_caches.sh
+++ global-scripts/configure_hf_caches.sh
@@ -26,6 +26,10 @@ export HF_MODULES_CACHE="$hf_cache_dir"/modules
 # Backward compatibility
 export HUGGINGFACE_ASSETS_CACHE="$hf_cache_dir"/assets
 export HUGGINGFACE_HUB_CACHE="$hf_cache_dir"/hub
-export TRANSFORMERS_CACHE="$hf_cache_dir"/hub
+# This one spits annoying deprecation warnings about using `HF_HOME`
+# even though we set that one to the same value. Assuming we always
+# use a recent enough `transformers` version, it should be fine to
+# leave this commented out.
+# export TRANSFORMERS_CACHE="$hf_cache_dir"/hub
 
 pop_curr_file
