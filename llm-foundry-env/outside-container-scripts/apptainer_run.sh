91da033fa0f658529365233492877101dfa2a908
janEbert <janpublicebert@posteo.net>
2024-04-03 14:04:49 +0200

    Unset more envvars
    On JSC sytems, `LESSOPEN` references a file that doesn't exist in the
    container, and thus causes unnecessary errors.

--- llm-foundry-env/outside-container-scripts/apptainer_run.sh
+++ llm-foundry-env/outside-container-scripts/apptainer_run.sh
@@ -61,7 +61,7 @@ fi
 
 # We unset a bunch of environment variables so they don't disturb our Apptainer.
 env -u BASH_ENV -u CC -u CFLAGS -u CMAKE_LIBRARY_PATH -u CMAKE_PREFIX_PATH \
-    -u CPATH -u CXX -u CXXFLAGS -u PYTHONPATH \
+    -u CPATH -u CXX -u CXXFLAGS -u LESSOPEN -u PYTHONPATH \
     apptainer run --nv \
     --bind "$scratch_dir"/triton-build-patch.py:/usr/lib/python3/dist-packages/triton/common/build.py \
     "$apptainer_file" "${args[@]}"
