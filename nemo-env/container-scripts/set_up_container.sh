01016a08addc6a004a809ff2ddb6405538b4a289
janEbert <janpublicebert@posteo.net>
2024-04-09 12:50:56 +0200

    Download NeMo HF tokenizers during setup

--- nemo-env/container-scripts/set_up_container.sh
+++ nemo-env/container-scripts/set_up_container.sh
@@ -28,6 +28,9 @@ else
     source "$venv_dir"/bin/activate
 fi
 
+# Download all HuggingFace tokenizers, that NeMo can use.
+python -c 'from transformers import AutoTokenizer; list(AutoTokenizer.from_pretrained(tok_name) for tok_name in ["gpt2", "bert-large-uncased", "bert-large-cased"])'
+
 # Clone and install the external repositories
 mkdir -p "$(dirname "$ext_repo_dir")"
 for _repo_uri in "${!repos[@]}"; do
