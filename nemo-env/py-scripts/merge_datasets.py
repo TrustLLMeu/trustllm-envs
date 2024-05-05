b9580352e96595dcdaa25f2dbef48c6b8ead59c6
ebert1 <ja.ebert@fz-juelich.de>
2024-05-06 00:36:52 +0200

    Add NeMo distributed dataset merging

---
+++ nemo-env/py-scripts/merge_datasets.py
@@ -0,0 +1,94 @@
+# Implementation adapted from `Megatron-LM/tools/merge_datasets.py` for
+# NeMo at revision ad53b1e38689a0ceed75ade7821f4e6c7554abb4.
+# This standalone version obviously does not benefit from updates to the
+# original version, but is thus supposed to be more stable.
+
+import os
+import sys
+import json
+import argparse
+
+from nemo.collections.nlp.data.language_modeling.megatron.indexed_dataset import (
+    MMapIndexedDataset,
+    MMapIndexedDatasetBuilder,
+    data_file_path as get_bin_path,
+    index_file_path as get_idx_path,
+)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    group = parser.add_argument_group(title="input data")
+    group.add_argument(
+        "--input",
+        type=str,
+        required=True,
+        help="Path to directory containing all document files to merge",
+    )
+
+    group = parser.add_argument_group(title="output data")
+    group.add_argument(
+        "--output-prefix",
+        type=str,
+        required=True,
+        help="Path to binary output file without suffix",
+    )
+
+    group = parser.add_argument_group(title="miscellaneous")
+    group.add_argument(
+        "--multimodal",
+        action="store_true",
+        help="Whether the datasets are assumed to be multimodal"
+    )
+
+    args = parser.parse_args()
+
+    assert os.path.isdir(
+        args.input
+    ), f"ERROR: {args.input} is not a directory or does not exist"
+
+    assert os.path.isdir(
+        os.path.dirname(args.output_prefix)
+    ), f"ERROR: {os.path.dirname(args.output_prefix)} is not a directory or does not exist"
+
+    return args
+
+
+def main():
+    args = get_args()
+
+    prefixes = set()
+    for basename in os.listdir(args.input):
+        prefix, ext = os.path.splitext(basename)
+
+        if prefix in prefixes:
+            continue
+
+        if not os.path.isfile(os.path.join(args.input, basename)):
+            continue
+
+        ext_pair = ".bin" if ext == ".idx" else ".idx"
+        assert os.path.isfile(
+            os.path.join(args.input, prefix) + ext_pair
+        ), f"ERROR: {ext_pair} file not provided for {os.path.join(args.input, prefix)}"
+
+        prefixes.add(prefix)
+
+    builder = None
+    for prefix in sorted(prefixes):
+        if builder is None:
+            dataset = MMapIndexedDataset(os.path.join(args.input, prefix))  # , multimodal=args.multimodal)
+            builder = MMapIndexedDatasetBuilder(
+                get_bin_path(args.output_prefix), dtype=dataset._index.dtype,  # multimodal=args.multimodal
+            )
+            del dataset
+
+        builder.merge_file_(os.path.join(args.input, prefix))
+
+    builder.finalize(get_idx_path(args.output_prefix))
+
+
+if __name__ == '__main__':
+
+    main()
---
+++ nemo-env/py-scripts/merge_datasets_patching.py
@@ -0,0 +1,77 @@
+# This version of dataset merging for NeMo was implemented on top of
+# `Megatron-LM/tools/merge_datasets.py` revision
+# ad53b1e38689a0ceed75ade7821f4e6c7554abb4.
+
+from argparse import ArgumentParser
+import importlib
+import os
+import sys
+from typing import Type
+
+import numpy
+
+from nemo.collections.nlp.data.language_modeling.megatron.indexed_dataset import (
+    MMapIndexedDataset as _MMapIndexedDataset,
+    MMapIndexedDatasetBuilder as _MMapIndexedDatasetBuilder,
+    data_file_path as get_bin_path,
+    index_file_path as get_idx_path,
+)
+
+
+class MMapIndexedDataset(_MMapIndexedDataset):
+    def __init__(self, path_prefix: str, multimodal: bool = False) -> None:
+        super().__init__(path_prefix)
+
+    @property
+    def index(self):
+        return self._index
+
+    def add_index(self, *args, **kwargs):
+        return self.merge_file_(*args, **kwargs)
+
+
+class MMapIndexedDatasetBuilder(_MMapIndexedDatasetBuilder):
+    def __init__(
+            self,
+            bin_path: str,
+            dtype: Type[numpy.number] = numpy.int32,
+            multimodal: bool = False,
+    ):
+        super().__init__(bin_path, dtype)
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--merge-script', required=True)
+    return parser.parse_known_args()
+
+
+def import_merge_datasets(merge_script: str):
+    module_name = 'merge_datasets'
+    spec = importlib.util.spec_from_file_location(module_name, merge_script)
+    merge_datasets = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = merge_datasets
+    spec.loader.exec_module(merge_datasets)
+    return merge_datasets
+
+
+def patch_imports(merge_datasets):
+    merge_datasets.MMapIndexedDataset = MMapIndexedDataset
+    merge_datasets.MMapIndexedDatasetBuilder = MMapIndexedDatasetBuilder
+    merge_datasets.get_bin_path = get_bin_path
+    merge_datasets.get_idx_path = get_idx_path
+
+
+def main():
+    args, passthrough_args = parse_args()
+    assert os.path.isfile(args.merge_script)
+
+    merge_datasets = import_merge_datasets(args.merge_script)
+    patch_imports(merge_datasets)
+
+    sys.argv = [args.merge_script] + passthrough_args
+    merge_datasets.main()
+
+
+if __name__ == '__main__':
+    main()
