7ed15f7d8a10d4fe695fab032174423441861a91
janEbert <janpublicebert@posteo.net>
2024-06-05 11:07:44 +0200

    Refactor partial outside software activation
    Can now be implemented per supercomputer machine.

---
+++ llm-foundry-env/jsc/_activate.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Partially configure the software environment used outside the
+# container for JSC machines.
+
+set -euo pipefail
+
+# Do not use these variables; they may be overwritten. Instead, use
+# `get_curr_file` or `get_curr_dir` after sourcing `get_curr_file.sh`.
+_curr_file="${BASH_SOURCE[0]:-${(%):-%x}}"
+_curr_dir="$(dirname "$_curr_file")"
+source "$_curr_dir"/../../global-scripts/get_curr_file.sh "$_curr_file"
+
+source "$(get_curr_dir)"/../../global_configuration.sh
+
+if [ "$SYSTEMNAME" = juwelsbooster ] \
+       || [ "$SYSTEMNAME" = juwels ] \
+       || [ "$SYSTEMNAME" = jurecadc ]; then
+    module purge
+    module load Stages/2024
+    module load GCC
+    if [ "$container_library" = apptainer ]; then
+        # Handle partitions without Apptainer-Tools module.
+        module try-load Apptainer-Tools
+        if ! [ "$(command -v apptainer)" ]; then
+            echo "Could not find Apptainer on JSC machine."
+            exit 1
+        fi
+    fi
+    module load NCCL
+fi
+
+pop_curr_file
--- llm-foundry-env/outside-container-scripts/activate.sh
+++ llm-foundry-env/outside-container-scripts/activate.sh
@@ -4,15 +4,18 @@
 
 set -euo pipefail
 
-module purge
-module load Stages/2024
-module load GCC Apptainer-Tools NCCL
-
 # Do not use these variables; they may be overwritten. Instead, use
 # `get_curr_file` or `get_curr_dir` after sourcing `get_curr_file.sh`.
 _curr_file="${BASH_SOURCE[0]:-${(%):-%x}}"
 _curr_dir="$(dirname "$_curr_file")"
 source "$_curr_dir"/../../global-scripts/get_curr_file.sh "$_curr_file"
+
+# JSC machine activation steps
+if [ "$SYSTEMNAME" = juwelsbooster ] \
+       || [ "$SYSTEMNAME" = juwels ] \
+       || [ "$SYSTEMNAME" = jurecadc ]; then
+    source "$(get_curr_dir)"/../jsc/_activate.sh
+fi
 
 source "$(get_curr_dir)"/../configuration.sh
 
