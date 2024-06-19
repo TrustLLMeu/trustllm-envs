641cb83823b2510cf78a065283cebe80083ffa86
janEbert <janpublicebert@posteo.net>
2024-06-19 16:29:38 +0200

    Update NeMo commit

--- nemo-env/configuration.sh
+++ nemo-env/configuration.sh
@@ -48,7 +48,7 @@ repos+=( 'https://github.com/NVIDIA/Megatron-LM.git a645f89671be698612170539f208
 # Main framework. Commit taken from old NeMo container
 # `/opt/NeMo/.git/logs/HEAD`.
 # This would execute `python -m pip install -e '.[all]'`
-repos+=( 'https://github.com/TrustLLMeu/NeMo.git 1195bbcd9346fd1afa527a2610ee9c9676d9c3ee [all]' )
+repos+=( 'https://github.com/TrustLLMeu/NeMo.git cb50e22278557cfecdb3960111ce68369f6aaab6 [all]' )
 # Launcher library. Commit taken from old NeMo container
 # `/opt/NeMo-Framework-Launcher/.git/logs/HEAD`.
 # This one is hardcoded to execute `python -m pip install -r
