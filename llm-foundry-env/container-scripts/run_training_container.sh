359dfaa7ac6fd606abdf47660cb80f4ba3706f56
janEbert <janpublicebert@posteo.net>
2024-09-03 15:46:24 +0200

    Automatically get and pad vocab size

--- llm-foundry-env/container-scripts/run_training_container.sh
+++ llm-foundry-env/container-scripts/run_training_container.sh
@@ -4,6 +4,12 @@
 
 set -euo pipefail
 
+# Do not use these variables; they may be overwritten. Instead, use
+# `get_curr_file` or `get_curr_dir` after sourcing `get_curr_file.sh`.
+_curr_file="${BASH_SOURCE[0]:-${(%):-%x}}"
+_curr_dir="$(dirname "$_curr_file")"
+source "$_curr_dir"/../../global-scripts/get_curr_file.sh "$_curr_file"
+
 _activated_container="${_ACTIVATED_CONTAINER:-0}"
 if ! ((_activated_container)); then
     echo 'Container has not been activated; please use' \
@@ -17,6 +23,10 @@ fi
 TRAIN_NUM_WORKERS="${TRAIN_NUM_WORKERS:-0}"
 EVAL_NUM_WORKERS="${EVAL_NUM_WORKERS:-0}"
 
+vocab_size="$((python -u "$(get_curr_dir)"/../py-scripts/get_vocab_size.py "$TOKENIZER_DIR"))"
+pad_vocab_size_to=128
+padded_vocab_size="$((vocab_size + pad_vocab_size_to - (vocab_size % pad_vocab_size_to)))"
+
 cd "$ext_repo_dir"/llm-foundry/scripts
 
 # Below is the LLM Foundry README quickstart example, modified to
@@ -29,6 +39,7 @@ cd "$ext_repo_dir"/llm-foundry/scripts
 # - use multiple CPUs for data processing (variables defined outside
 #   script),
 # - use FlashAttention-2,
+# - use a padded vocab size,
 # - save checkpoints to SCRATCH.
 
 # Train a model for 10 batches
@@ -47,6 +58,7 @@ python -u -m composer \
     eval_loader.num_workers="$EVAL_NUM_WORKERS" \
     eval_loader.dataset.split=val \
     model.attn_config.attn_impl=flash \
+    model.vocab_size="$padded_vocab_size" \
     max_duration=10ba \
     eval_interval=0 \
     save_folder="$MODEL_CHECKPOINT_DIR"
@@ -71,3 +83,5 @@ python -u -m composer \
 #   --prompts \
 #     "The answer to life, the universe, and happiness is" \
 #     "Here's a quick recipe for baking chocolate chip cookies: Start by"
+
+pop_curr_file
