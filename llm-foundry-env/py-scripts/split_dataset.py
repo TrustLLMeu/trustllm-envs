09cb28196011baf05ccf8a96873347a7e58f7123
janEbert <janpublicebert@posteo.net>
2024-09-17 14:43:02 +0200

    Clean up

--- llm-foundry-env/py-scripts/split_dataset.py
+++ llm-foundry-env/py-scripts/split_dataset.py
@@ -2,32 +2,39 @@
 # (https://github.com/TrustLLMeu/trustllm-envs/pull/2).
 
 from argparse import ArgumentParser
+import functools
+import multiprocessing as mp
+import os
 
 import numpy as np
 from streaming import MDSWriter, StreamingDataset
-import os
 import tqdm
-import multiprocessing as mp
-from functools import partial
 
-# Define worker function to handle writing data
-def write_data(writer_kwargs, ds, idx_chunk, worker_id):
+
+def write_data(writer_kwargs, ds, indices_shard, rank):
     assert 'out' in writer_kwargs
-    writer_kwargs['out'] = os.path.join(writer_kwargs['out'], str(worker_id))
+    writer_kwargs['out'] = os.path.join(writer_kwargs['out'], str(rank))
     with MDSWriter(**writer_kwargs) as writer:
-        for idx in tqdm.tqdm(idx_chunk):
-            writer.write(ds.get_item(idx))
+        for index in tqdm.tqdm(indices_shard):
+            writer.write(ds.get_item(index))
+
 
 def parallelize_writing(writer_kwargs, ds, indices, num_workers):
-    # Split indices into chunks, excluding remainder
-    idx_chunks = np.array_split(indices, num_workers)
+    # Create indices shards.
+    indices_shards = np.array_split(indices, num_workers)
+    # Create a list of (shard, rank) pairs to pass to the workers.
+    worker_args = [
+        (indices_shard, rank)
+        for (rank, indices_shard) in enumerate(indices_shards)
+    ]
 
-    # Use multiprocessing to parallelize writing
+    # Write in parallel.
     with mp.Pool(processes=num_workers) as pool:
-        # Create a list of (chunk, worker_id) pairs to pass to the workers
-        worker_args = [(idx_chunks[i], i) for i in range(len(idx_chunks))]
-        # Pass worker_id and chunk to each worker using starmap
-        pool.starmap(partial(write_data, writer_kwargs, ds), worker_args)
+        pool.starmap(
+            functools.partial(write_data, writer_kwargs, ds),
+            worker_args,
+        )
+
 
 def parse_args():
     parser = ArgumentParser()
@@ -139,46 +146,67 @@ def run_split(
 
     rng = np.random.default_rng(seed)
     threshold = min(right_max / len(ds), right_prob)
-    right_idx = []
-    while True:
-        next_id = rng.geometric(threshold)
-        if len(right_idx) > 0:
-            next_id += right_idx[-1]
-        else:
-            next_id -= 1
-        right_idx.append(next_id)
 
-        if right_idx[-1] >= len(ds):
-            right_idx.pop()
-            break
-    left_idx = np.setdiff1d(np.arange(len(ds)), right_idx, assume_unique=True)
-    
-    print('total samples:', len(ds))
-    print('train samples:', len(left_idx))
-    print('val samples:', len(right_idx))
-    assert all(np.sort(np.append(left_idx, right_idx)) == range(len(ds)))
-    assert len(np.unique(left_idx)) == len(left_idx)
-    assert len(np.unique(right_idx)) == len(right_idx)
+    # This is the number of samples until the next index in the "right"
+    # set. Always >= 1.
+    num_samples_until_right = rng.geometric(threshold)
+    # Convert number of samples to index.
+    right_index = num_samples_until_right - 1
+    right_indices = [right_index]
+    while right_indices[-1] < len(ds):
+        # This is the number of samples until the next index in the
+        # "right" set. Always >= 1.
+        num_samples_until_right = rng.geometric(threshold)
+        # Use number of samples as "distance" to compute next index.
+        right_index = right_indices[-1] + num_samples_until_right
+        right_indices.append(right_index)
 
-    # Parallelize writing for left and right indices
-    left_writer_kwargs = {
+    # Remove last index that was too large and caused us to exit the
+    # while-loop.
+    right_indices.pop()
+
+    all_indices = np.arange(len(ds))
+    left_indices = np.setdiff1d(
+        all_indices,
+        right_indices,
+        assume_unique=True,
+    )
+
+    print('number of total samples:', len(ds))
+    print('number of left samples:', len(left_indices))
+    print('number of right samples:', len(right_indices))
+    assert np.all(
+        np.sort(np.append(left_indices, right_indices)) == all_indices,
+    )
+    del all_indices
+    assert len(np.unique(left_indices)) == len(left_indices)
+    assert len(np.unique(right_indices)) == len(right_indices)
+
+    common_kwargs = {
         'columns': columns,
-        'out': left_path,
         'compression': compression,
     }
-    right_writer_kwargs = {
-        'columns': columns,
-        'out': right_path,
-        'compression': compression,
-    }
+    left_writer_kwargs = common_kwargs.copy()
+    left_writer_kwargs['out'] = left_path
+    right_writer_kwargs = common_kwargs.copy()
+    right_writer_kwargs['out'] = right_path
 
-    # Parallelize writing left_idx
-    print('Writing left_idx...')
-    parallelize_writing(left_writer_kwargs, ds, left_idx, num_workers=num_workers)
+    print('Writing left split...')
+    parallelize_writing(
+        left_writer_kwargs,
+        ds,
+        left_indices,
+        num_workers=num_workers,
+    )
 
-    # Parallelize writing right_idx
-    print('Writing right_idx...')
-    parallelize_writing(right_writer_kwargs, ds, right_idx, num_workers=num_workers)
+    print('Writing right split...')
+    parallelize_writing(
+        right_writer_kwargs,
+        ds,
+        right_indices,
+        num_workers=num_workers,
+    )
+
 
 if __name__ == '__main__':
     args = parse_args()
