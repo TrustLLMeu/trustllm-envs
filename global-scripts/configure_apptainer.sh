7d9a2e72697647dec402f1f0f46e80eb3457d695
janEbert <janpublicebert@posteo.net>
2025-01-14 10:03:06 +0100

    Unconditionally use backward-compatible envvars
    Checking the name of `apptainer_bin` is not rigorous enough, so we just
    go with Occam's razor.

--- global-scripts/configure_apptainer.sh
+++ global-scripts/configure_apptainer.sh
@@ -15,16 +15,12 @@ source "$(get_curr_dir)"/../global_configuration.sh
 
 mkdir -p "$apptainer_cache_dir"
 export APPTAINER_CACHEDIR="$apptainer_cache_dir"
-if [ "$apptainer_bin" = singularity ]; then
-    # Also export old name for backward-compatibility.
-    export SINGULARITY_CACHEDIR="$APPTAINER_CACHEDIR"
-fi
+# Also export old name for backward-compatibility.
+export SINGULARITY_CACHEDIR="$APPTAINER_CACHEDIR"
 
 mkdir -p "$apptainer_tmp_dir"
 export APPTAINER_TMPDIR="$apptainer_tmp_dir"
-if [ "$apptainer_bin" = singularity ]; then
-    # Also export old name for backward-compatibility.
-    export SINGULARITY_TMPDIR="$APPTAINER_TMPDIR"
-fi
+# Also export old name for backward-compatibility.
+export SINGULARITY_TMPDIR="$APPTAINER_TMPDIR"
 
 pop_curr_file
