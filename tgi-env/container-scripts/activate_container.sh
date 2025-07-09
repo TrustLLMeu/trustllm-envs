33866a345e148c0de6da6c77370fcde7ba290218
janEbert <janpublicebert@posteo.net>
2025-07-09 12:03:30 +0200

    Add TGI environment

---
+++ tgi-env/container-scripts/activate_container.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Configure the software environment used inside the container.
+
+set -euo pipefail
+
+# Do not use these variables; they may be overwritten. Instead, use
+# `get_curr_file` or `get_curr_dir` after sourcing `get_curr_file.sh`.
+_curr_file="${BASH_SOURCE[0]:-${(%):-%x}}"
+_curr_dir="$(dirname "$_curr_file")"
+source "$_curr_dir"/../../global-scripts/get_curr_file.sh "$_curr_file"
+
+source "$(get_curr_dir)"/../parent_env.sh
+if [ "$#" -gt 1 ] && [ "$1" = __inherit__ ]; then
+    # If we are inheriting from this environment, we use the parent of
+    # this environment for further propagation, but all other
+    # locations use the child environment.
+    pop_curr_file
+
+    _curr_file="$2"
+    _curr_dir="$(dirname "$_curr_file")"
+    source "$_curr_dir"/../../global-scripts/get_curr_file.sh "$_curr_file"
+    _args=( "${@:3}" )
+else
+    _args=( "$@" )
+fi
+
+if (("${DEBUG_TRUSTLLM_ENVS:-0}")); then
+    printf '  in: %s\n    curr_file = %s\n' \
+           "${BASH_SOURCE[0]:-${(%):-%x}}" \
+           "$(get_curr_file)"
+fi
+
+# -----
+
+# Re-use parent environment's script.
+source "$parent_env_dir"/container-scripts/activate_container.sh \
+       __inherit__ "$(get_curr_file)" \
+       "${_args[@]}"
+
+pop_curr_file
---
+++ tgi-env/parent_env.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This script contains a path to the parent environment of the PyTorch
+# environment.
+
+set -euo pipefail
+
+# Do not use these variables; they may be overwritten. Instead, use
+# `get_curr_file` or `get_curr_dir` after sourcing `get_curr_file.sh`.
+_curr_file="${BASH_SOURCE[0]:-${(%):-%x}}"
+_curr_dir="$(dirname "$_curr_file")"
+source "$_curr_dir"/../global-scripts/get_curr_file.sh "$_curr_file"
+
+# Directory of this environment.
+curr_env_dir="$(get_curr_dir)"
+
+# Directory of environment this one bases on.
+parent_env_dir="$(get_curr_dir)"/../pytorch-env
+
+pop_curr_file
