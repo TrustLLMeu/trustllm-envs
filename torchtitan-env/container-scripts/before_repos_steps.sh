5328665e488b0f424e78bc76b4f798ac22fd2645
janEbert <janpublicebert@posteo.net>
2025-08-13 12:34:12 +0200

    Manually install latest PyTorch

--- torchtitan-env/container-scripts/before_repos_steps.sh
+++ torchtitan-env/container-scripts/before_repos_steps.sh
@@ -12,7 +12,7 @@ source "$_curr_dir"/../../global-scripts/get_curr_file.sh "$_curr_file"
 
 source "$(get_curr_dir)"/configure_pip_install_variables.sh
 
-# python -m pip "${_pip_install_args[@]}" 'torch==2.7.1'
+python -m pip "${_pip_install_args[@]}" 'torch==2.8.0'
 
 # Install Rust
 _cpu_arch="$(uname -m)"
