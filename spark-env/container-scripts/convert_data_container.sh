c68187a231d254a315ce0c72fc68456e099b18d4
janEbert <janpublicebert@posteo.net>
2025-11-04 17:09:12 +0100

    Add Spark data conversion scripts

---
+++ spark-env/container-scripts/convert_data_container.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Start a conversion from data in one format to converted data in
+# another format.
+
+set -euo pipefail
+
+_curr_file="${BASH_SOURCE[0]:-${(%):-%x}}"
+_curr_dir="$(dirname "$_curr_file")"
+source "$_curr_dir"/../../global-scripts/get_curr_file.sh "$_curr_file"
+
+_activated_container="${_ACTIVATED_CONTAINER:-0}"
+if ! ((_activated_container)); then
+    echo 'Container has not been activated; please use' \
+         "\`bash container_run.sh\` to run container scripts."
+    exit 1
+fi
+
+# -----
+
+mkdir -p "$(dirname "$OUTPUT_DATA_DIR")"
+
+INPUT_FORMAT="${INPUT_FORMAT:-parquet}"
+OUTPUT_FORMAT="${OUTPUT_FORMAT:-parquet}"
+OUTPUT_COMPRESSION="${OUTPUT_COMPRESSION:-zstd}"
+MY_SPARK_CACHE_DIR="${MY_SPARK_CACHE_DIR:-"$cache_dir"}"
+
+export SPARK_LOCAL_DIRS="$MY_SPARK_CACHE_DIR"/spark-"$SLURM_JOB_ID"
+if ((NODE_RANK)); then
+    spark_work_dir="$MY_SPARK_CACHE_DIR"/spark-"$NODE_RANK"-"$SLURM_JOB_ID"
+    spark-class org.apache.spark.deploy.worker.Worker \
+                spark://"$MASTER_ADDR":"$MASTER_PORT" \
+                --memory "$AVAILABLE_MEM_GB"G \
+                --work-dir "$spark_work_dir"
+else
+    spark-class org.apache.spark.deploy.master.Master \
+                --host "$MASTER_ADDR" --port "$MASTER_PORT" &
+    master_proc="$!"
+
+    python -u "$(get_curr_dir)"/../py-scripts/convert_data.py \
+           --dist-input-files="$INPUT_DATA_FILES" \
+           --dist-input-files-glob="$INPUT_DATA_FILES_GLOB" \
+           --output-dir="$OUTPUT_DATA_DIR" \
+           --local-dir "$SPARK_LOCAL_DIRS" \
+           --event-dir "$MY_SPARK_CACHE_DIR"/spark-events-"$SLURM_JOB_ID" \
+           --available-mem-gb "$AVAILABLE_MEM_GB" \
+           --input-format "$INPUT_FORMAT" \
+           --output-format "$OUTPUT_FORMAT" \
+           --output-compression "$OUTPUT_COMPRESSION"
+
+    kill -s KILL "$master_proc"
+fi
---
+++ spark-env/py-scripts/convert_data.py
@@ -0,0 +1,154 @@
+from argparse import ArgumentParser
+import atexit
+import glob
+import math
+import os
+import tempfile
+
+from pyspark.sql import SparkSession
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--dist-input-files',
+        help='Colon-separated list of input files to process',
+    )
+    parser.add_argument(
+        '--dist-input-files-glob',
+        help='Glob of input files to process',
+    )
+    parser.add_argument(
+        '--local-dir',
+        default=os.getenv('SPARK_LOCAL_DIRS', tempfile.gettempdir()),
+    )
+    parser.add_argument(
+        '--event-dir',
+        default=os.path.join(tempfile.gettempdir(), 'spark-events'),
+    )
+    parser.add_argument('--available-mem-gb', type=float)
+    parser.add_argument('--output-dir', required=True)
+    parser.add_argument(
+        '--num-shards',
+        type=int,
+        help='Uniformly shard across all input files into this many shards',
+    )
+    parser.add_argument('--rank', type=int)
+    parser.add_argument(
+        '--input-format',
+        choices=['parquet', 'json'],
+        default='parquet',
+        help='Format of the input data, i.e., when reading.',
+    )
+    parser.add_argument(
+        '--output-format',
+        choices=['parquet', 'json'],
+        default='parquet',
+        help='Format of the output data, i.e., when writing.',
+    )
+    parser.add_argument(
+        '--output-compression',
+        default='zstd',
+        help=(
+            'Compression to apply to the output data, i.e., when writing. '
+            '"none" or "" for no compression.'
+        ),
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    assert args.dist_input_files or args.dist_input_files_glob, (
+        'need either `--dist-input-files` or `--dist-input-files-glob` '
+        'to be specified'
+    )
+    assert (
+        args.num_shards is None and args.rank is None
+        or args.num_shards is not None and args.rank is not None
+    ), 'cannot give only one of `--num-shards` and `--rank`; please set both'
+
+    world_size = int(os.environ['WORLD_SIZE'])
+
+    spark = SparkSession.builder.master(
+        f'spark://{os.environ["MASTER_ADDR"]}:{os.environ["MASTER_PORT"]}',
+    ).config(
+        'spark.local.dir',
+        args.local_dir,
+    ).config(
+        'spark.eventLog.dir',
+        args.event_dir,
+    ).config(
+        'spark.serializer',
+        'org.apache.spark.serializer.KryoSerializer',
+    )
+    if args.available_mem_gb:
+        memory_gb = args.available_mem_gb
+        spark = spark.config(
+            'spark.executor.memory',
+            f'{math.floor(memory_gb * 0.48)}g',
+        ).config(
+            'spark.driver.memory',
+            f'{round(memory_gb * 0.25)}g',
+        ).config(
+            'spark.driver.maxResultSize',
+            f'{round(memory_gb * 0.24)}g',
+        )
+
+    spark = spark.getOrCreate()
+
+    def close_client():
+        spark.sparkContext.stop()
+        spark.stop()
+
+    atexit.register(close_client)
+
+    os.makedirs(os.path.dirname(args.output_dir), exist_ok=True)
+
+    input_files = []
+    if args.dist_input_files:
+        input_files.extend(args.dist_input_files.split(':'))
+    if args.dist_input_files_glob:
+        input_files.extend(sorted(glob.glob(args.dist_input_files_glob)))
+
+    if args.num_shards is not None:
+        input_files = input_files[args.rank::args.num_shards]
+
+    print(f'now reading {args.input_format}')
+    if args.input_format == 'parquet':
+        df = spark.read.parquet(*input_files)
+    elif args.input_format == 'json':
+        df = spark.read.json(input_files)
+    else:
+        print(f'unhandled data input format {args.input_format}...')
+    print('read parquet, now coalescing')
+    df = df.coalesce(world_size)
+
+    output_compression = (
+        args.output_compression
+        if args.output_compression not in ['', 'none']
+        else None
+    )
+    print(f'coalesced, now writing {args.output_format}')
+    if args.output_format == 'parquet':
+        df.write.parquet(
+            args.output_dir,
+            mode='overwrite',
+            compression=output_compression,
+        )
+    elif args.output_format == 'json':
+        df.write.json(
+            args.output_dir,
+            mode='overwrite',
+            compression=output_compression,
+        )
+    else:
+        print(f'unhandled data output format {args.output_format}...')
+    print('done')
+
+    close_client()
+    atexit.unregister(close_client)
+
+
+if __name__ == '__main__':
+    main()
